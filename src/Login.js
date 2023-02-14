c930ec93ab97c38d15307cc4b7c13449bb399972
Anu Oyebanjo <anuoyebanjodunmi660@gmail.com>
2023-02-14 13:01:36 +0100

    first commit

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import "./Login.css"
+import {auth} from "./firebase";
 
 function Login() {
+  const navigate = useNavigate();
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+
+const signIn = e => {
+  e.preventDefault();
+
+  auth
+      .signInWithEmailAndPassword(email, password)
+      .then(auth => {
+       // navigate.push("/")
+        navigate("/")
+      })
+      .catch(error => alert(error.message))
+
+
+
+
+  // some fancy firebase login shittt...
+}
+
+
+
+const register = e => {
+  e.preventDefault();
+
+
+  auth
+  .createUserWithEmailAndPassword(email, password)
+  .then((auth) => {
+    //it successfully created a new user with email and password
+     if(auth) {
+      //navigate.push("/")
+      navigate("/")
+     }   
+  })
+  .catch(error => alert(error.message))
+
+  
+
+
+
+
+
+  // some fancy firebase register shittt...
+}
+
+
+
+
   return (
     <div className="login">
         <Link to="/">
@@ -13,18 +66,22 @@ function Login() {
 
             <form>
                 <h5>E-mail</h5>
-                <input type="text" />
+                <input type="text" value={email} 
+                onChange={e => setEmail(e.target.value)} />
 
                 <h5>Password</h5>
-                <input type="password" />
+                <input type="password" value={password} 
+                onChange={e => setPassword(e.target.value)} />
 
-                <button className="login__signInButton">Sign In</button>
+                <button type="submit" onClick={signIn}
+                className="login__signInButton">Sign In</button>
             </form>
 
             <p>
             By signing-in an account, you agree to Amazon's Conditions of Use and Privacy Notice.
             </p>
-            <button className="login__registerButton">Create your Amazon Account</button>
+            <button onClick={register}
+            className="login__registerButton">Create your Amazon Account</button>
 
         </div>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyA47q265FGHkG-tPMveEvatNCvESMntsVw",
     authDomain: "anu--clone.firebaseapp.com",
@@ -6,3 +11,11 @@ const firebaseConfig = {
     messagingSenderId: "286849021286",
     appId: "1:286849021286:web:7643e8be4fb779f78c04a7"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+  const auth = firebase.auth();
+
+  export {db, auth};
